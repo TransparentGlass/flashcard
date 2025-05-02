5395b930280d7706aa97512931808faebc47f5f9
glass <toshilikescookies@gmail.com>
2025-05-02 10:13:14 +0800

    feat(flashcard): add flashcard

---
+++ src/main/java/flashcard/flashcard.java
@@ -0,0 +1,34 @@
+package flashcard;
+
+public class flashcard{
+    flashcard next;
+    flashcard prev;
+    String front;
+    String back;
+
+    public flashcard(String front, String back){
+        this.next = null;
+        this.prev = null;
+        this.front = front;
+        this.back = back;
+    }
+
+    public void setQuestion(String q){
+        this.front = q;
+    }
+
+    public void setAnswer(String a){
+        this.back = a;
+    }
+
+    public String getAnswer(){
+        return this.back;
+    }
+
+    public String getQuestion(){
+        return this.front;
+
+    }
+    
+
+}
