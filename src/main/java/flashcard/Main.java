42e33428487e8a86ead2ec56bae84349868802cf
glass <89008737+TransparentGlass@users.noreply.github.com>
2025-05-12 09:37:05 +0800

    refact(buttonCC, sprites): rename button cc to menu and add a trimmed menu sprites

--- src/main/java/flashcard/Main.java
+++ src/main/java/flashcard/Main.java
@@ -1,15 +1,13 @@
 package flashcard;
 
-
-import flashcard.GUI.jilMenu;
+import flashcard.GUI.menu;
 import flashcard.tests.testDeck;
 import flashcard.tests.testStudyMode;
 
 public class Main {
     public static void main(String[] args) {
  
-        jilMenu menu = new jilMenu();
-
+        menu menu = new menu();
         menu.init();
 
         testDeck newDeck = new testDeck();
